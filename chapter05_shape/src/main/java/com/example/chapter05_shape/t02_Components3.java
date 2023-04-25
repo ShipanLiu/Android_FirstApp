a345e6b8e8fbccfb0faa3a7d4eda8bf0dddcab45
Shipan <liushipan1998@gmail.com>
2023-04-25 19:27:46 +0200

    25.04 SQL update

--- chapter05_shape/src/main/java/com/example/chapter05_shape/t02_Components3.java
+++ chapter05_shape/src/main/java/com/example/chapter05_shape/t02_Components3.java
@@ -3,12 +3,23 @@ package com.example.chapter05_shape;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class t02_Components3 extends AppCompatActivity {
+
+    // Spinner dropdown
+    private Spinner spinner_dropdown;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.t02_components3);
+
+        // Spinner dropdown
+        Spinner spinner_dropdown = findViewById(R.id.sp_dropdown1);
+
+
+
     }
 }
